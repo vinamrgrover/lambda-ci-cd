79f11d1d624c128e43fd2485d18fde47127eae2e
Vinamr Grover <vinamrgrover@gmail.com>
2023-10-30 00:44:22 +0530

    updated function code : changed response format

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import requests
 def lambda_handler(event = None, context = None) -> str:
     url = 'https://random-word-api.herokuapp.com/word?length=5'
     response = requests.get(url)
-    return ''.join(response.json())
+    return ''.join(response.json()).upper()
 
